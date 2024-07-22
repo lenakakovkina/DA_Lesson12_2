eb2c1f2a7d6adf7081c2e934135b984d34d4ad50
Olena <elenakakovkina89@gmail.com>
2024-07-22 12:43:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿var marksPerSubject = new Dictionary<string, int>()
+{
+    {"math",10},
+    {"chemistry",8 },
+    {"literature",5 },
+    {"geography",4 },
+    {"physics",9},
+    {"physical education",11 }
+};
+var marksForRetest = marksPerSubject.Where(mark => mark.Value < 6);
+Console.WriteLine("The next marks need to be redone:");
+foreach (var mark in marksForRetest)
+{
+    Console.WriteLine($"{mark.Key}:{mark.Value}");
+}
